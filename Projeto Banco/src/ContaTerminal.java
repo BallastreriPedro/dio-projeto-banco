140cf9d0fe7ccc068e5ac6b707b46e9a27e55010
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-18 13:09:06 -0300

    Projeto Finalizado

--- Projeto Banco/src/ContaTerminal.java
+++ Projeto Banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Digite o Numero da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o Numero da Agencia: ");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o Nome do Cliente: ");
+        String cliente = scanner.next();
+
+        System.out.print("Digite o Saldo da Conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + cliente + ", Obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + ", e seu saldo " + saldo + " já está disponível para saque");
+
+        
     }
 }
